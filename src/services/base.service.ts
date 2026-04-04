8a48ce192833d1ff18f4514f316055ea3854bfcf
John Ruiz <jaetr2523@gmail.com>
2026-04-04 09:49:37 -0400

    I added a new component called MapView with the pasted tutorial code in block. I also did Supabase stuff, thanks mehreen

--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -4,8 +4,8 @@ import type { Database } from '../types/database.types'
 
 export abstract class BaseService {
   protected static supabase: SupabaseClient<Database> = createClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL!, //never do this
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! //never do this 
   )
 
   protected supabase: SupabaseClient<Database>
