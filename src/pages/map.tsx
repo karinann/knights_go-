5c39ff5e77e0a4dd723ad6a03aeaa0391e6339c8
karina <karinann514@gmail.com>
2026-04-10 12:55:47 -0400

    Relinked home.css from map.tsx

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic';
-import styles from './home/home.module.css';
+import styles from '../styles/home.module.css';
 
 const MapView = dynamic(() => import('../components/Map/MapView'), {
   ssr: false,
