80931c60522e926e107c754cff039ad890778492
karina <karinann514@gmail.com>
2026-04-09 22:28:46 -0400

    Moved map out of folder for simplified site navigation

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 
-const MapView = dynamic(() => import('../../components/Map/MapView'), {
+const MapView = dynamic(() => import('../components/Map/MapView'), {
   ssr: false,
 });
 
