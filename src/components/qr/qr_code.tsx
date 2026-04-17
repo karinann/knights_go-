98ab3b1e4ef390c6b95e1b62ae5bc7ef3dec869a
allis45 <allisonsique5@gmail.com>
2026-04-17 13:42:22 -0400

    changed layout for scanner and added beep

--- src/components/qr/qr_code.tsx
+++ src/components/qr/qr_code.tsx
@@ -1,24 +1,26 @@
 import { Scanner } from '@yudiel/react-qr-scanner';
 
-function qr() {
-  const handleScan = (detectedCodes: any[]) => {
-    console.log('Detected codes:', detectedCodes);
-    // detectedCodes is an array of IDetectedBarcode objects
-    detectedCodes.forEach(code => {
-      console.log(`Format: ${code.format}, Value: ${code.rawValue}`);
-    });
+function qr(){
+  const handleScan = (detectedCodes: any[]) => {console.log('Detected codes:', detectedCodes);
+     // detectedCodes is an array of IDetectedBarcode objects
+      detectedCodes.forEach(code=> {console.log('Format: ${code.format}, Value: ${code.rawValue}'); 
+    }); 
   };
 
-  return (
-    <Scanner
-      onScan={handleScan}
-      onError={(error) => console.error(error)}
-      constraints={{
-        facingMode: 'environment',
-        aspectRatio: 1,
-      }}
-    />
-  );
+
+return ( 
+  <Scanner
+  onScan={(codes) => {
+    codes.forEach(code => {
+      console.log("QR:", code.rawValue);
+    });
+  }}
+  onError={(err) => console.error(err)}
+  constraints={{
+    facingMode: "environment",
+  }}
+/>
+); 
 }
 
 export default qr;
--- src/pages/qr_page.tsx
+++ src/pages/qr_page.tsx
@@ -1,26 +1,26 @@
+import dynamic from 'next/dynamic'; 
+import BottomNav from '@/components'; 
+import styles from '../styles/home.module.css'; 
+import styles2 from '../../styles/BottomNav.module.css'; 
+import { useState } from "react";
 
-import dynamic from 'next/dynamic';
-import BottomNav from '@/components';
-import styles from '../styles/home.module.css';
-import styles2 from '../../styles/BottomNav.module.css';
-
-
-
-const Scanner = dynamic(
-  () => import('@yudiel/react-qr-scanner').then((mod) => mod.Scanner),
-  { ssr: false }
-);
-
+const Scanner = dynamic( () => import('@yudiel/react-qr-scanner').then((mod) => mod.Scanner), { ssr: false } );
 
 export default function qr_page() {
   return (
     <div className={styles.page}>
-      <div className ={styles.scannerContainer}>
-      <Scanner
-      onScan={(result) => console.log(result)}
-    />
-    </div>
-    <BottomNav />
+      <div className={styles.scannerWrapper}>
+        <Scanner
+          onScan={(codes) => {
+            codes.forEach(code => console.log(code.rawValue));
+          }}
+          constraints={{ facingMode: "environment" }}
+        />
+        <div className={styles.overlay} />
+        <div className={styles.scanBox} />
+      </div>
+
+      <BottomNav />
     </div>
   );
 }
